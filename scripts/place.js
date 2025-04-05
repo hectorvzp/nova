50ff011ce4fd7b4a5efa761ee1a8c7731aea7950
hectorvzp <elderzambranoperez@gmail.com>
2025-04-04 23:43:40 -0300

    dfdf

---
+++ scripts/place.js
@@ -0,0 +1,25 @@
+// DATA PARA FOOTER//
+const last = document.getElementById("lasti");
+const oLastModif = new Date(document.lastModified);
+const resultado = oLastModif.toLocaleString();
+last.innerHTML = resultado;
+
+const temperatura = 10;
+const vento = 5;
+
+const tempeCampo = document.querySelector(".temperatura");
+tempeCampo.innerHTML += `${temperatura}°C`;
+document.querySelector(".vento").innerHTML += `${vento} km/h`;
+
+calculateWindChill = (vento, temperatura) => {
+  const willChill =
+    13.12 +
+    0.6215 * temperatura -
+    11.37 * (vento * 0.16) +
+    0.3965 * temperatura * (vento * 0.16);
+  return willChill.toFixed(1);
+};
+
+if ((temperatura <= 10 || vento > 4, 8)) {
+  document.querySelector(".wind").innerHTML += `${calculateWindChill(vento,temperatura)} °C`
+}
--- week2/scripts/book.js
+++ week2/scripts/book.js
@@ -1,40 +1,54 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
+// const input = document.querySelector("#favchap");
+// const button = document.querySelector("button");
+// const list = document.querySelector("#list");
 
-const creaApagar = () => {
-  const apagar = document.createElement("button");
-  apagar.textContent = "❌";
-  apagar.classList.add("apagar");
-  return apagar;
-};
-const creaLi = () => {
-  const li = document.createElement("li");
-  return li;
-};
-function printarLivros() {
-  button.addEventListener("click", () => {
-    if (input.value != "") {
-      const li = creaLi();
-      const apagar = creaApagar();
-      li.textContent = input.value;
-      li.append(apagar);
-      list.append(li);
-      input.value = "";
-      input.focus();
-    } else {
-      return;
-    }
-  });
-}
-function clicarApagar() {
-  document.addEventListener("click", (e) => {
-    const el = e.target;
-    if (el.classList.contains("apagar")) {
-      el.parentElement.remove();
-    }
-  });
-}
+// const creaApagar = () => {
+//   const apagar = document.createElement("button");
+//   apagar.textContent = "❌";
+//   apagar.classList.add("apagar");
+//   return apagar;
+// };
+// const creaLi = () => {
+//   const li = document.createElement("li");
+//   return li;
+// };
+// function printarLivros() {
+//   button.addEventListener("click", () => {
+//     if (input.value != "") {
+//       const li = creaLi();
+//       const apagar = creaApagar();
+//       li.textContent = input.value;
+//       li.append(apagar);
+//       list.append(li);
+//       input.value = "";
+//       input.focus();
+//     } else {
+//       return;
+//     }
+//   });
+// }
+// function clicarApagar() {
+//   document.addEventListener("click", (e) => {
+//     const el = e.target;
+//     if (el.classList.contains("apagar")) {
+//       el.parentElement.remove();
+//     }
+//   });
+// }
 
-printarLivros();
-clicarApagar();
+// printarLivros();
+// clicarApagar();
+let scores = [100, 72, 83, 94, 88, 87];
+let accumulator = 0;
+let count = 0;
+scores.forEach(score => {
+  if (score > 87) {
+    accumulator += score;
+    count ++;
+  }
+});
+if ( count > 0 ) {
+  console.log(accumulator/count); 
+} else {
+  console.log("No scores reported.");
+}
